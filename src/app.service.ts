d09928e117b6160d043d4bdb4bf0e51f0fab10fd
Wissal Adday <wissaladday199@gmail.com>
2023-12-22 16:11:35 +0100

    test-workflow

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello this is a changeable thing ';
+    return 'Testing the workflow actions';
   }
 }
